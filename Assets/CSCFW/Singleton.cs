e1b2c5c34725cd7aa9f1a45c6e96d52db50705cd
zisi.mo <zisi.mo@mihoyo.com>
2018-09-29 15:56:09 +0800

    Singleton

---
+++ Assets/CSCFW/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace CSCFW
+{
+	public class Singleton<T> where T : class
+	{
+		private static T _instance = null;
+
+		static Singleton(){}
+
+		public static void Create()
+		{
+			if (null == _instance)
+			{
+				_instance = Activator.CreateInstance<T>();
+			}
+		}
+
+		public static void Destroy()
+		{
+			_instance = null;
+		}
+
+		public static T Instance
+		{
+			get
+			{
+				return _instance;
+			}
+		}
+	}
+}
+
